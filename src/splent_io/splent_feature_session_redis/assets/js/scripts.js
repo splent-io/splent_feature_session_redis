c21c18b6f7e2bfa73350e4ac055926547922f525
drorganvidez <davromorg@alum.us.es>
2026-04-02 17:06:34 +0000

    chore: release splent_io/splent_feature_session_redis to v1.0.7

--- src/splent_io/splent_feature_session_redis/assets/js/scripts.js
+++
@@ -1,1 +0,0 @@
-console.log("Hi, I am a script loaded from splent_feature_session_redis module");
--- src/splent_io/splent_feature_session_redis/seeders.py
+++
@@ -1,6 +0,0 @@
-from splent_framework.seeders.BaseSeeder import BaseSeeder
-
-
-class SplentFeatureSessionRedisSeeder(BaseSeeder):
-    def run(self):
-        self.seed([])
